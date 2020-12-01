8f2ef83f967a819aafc808a1af0ce1aa93e55d8e
Euen <euen@catalx.io>
2020-12-01 13:15:33 -0300

    Add presence to display active users

--- lib/fiar_reloaded_web/live/user_live/index.ex
+++ lib/fiar_reloaded_web/live/user_live/index.ex
@@ -4,9 +4,38 @@ defmodule FiarReloadedWeb.UserLive.Index do
   alias FiarReloaded.Repo.Users
   alias FiarReloaded.Repo.Schemas.User
 
+  alias FiarReloadedWeb.Presence
+  alias FiarReloaded.PubSub
+
+  @presence "fiar_reloaded:presence"
+
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, users: list_users())}
+  def mount(_params, session, socket) do
+    user =
+      if connected?(socket) and !is_nil(session["user_id"]) do
+        Phoenix.PubSub.subscribe(PubSub, @presence)
+
+        user =
+          session["user_id"]
+          |> Users.get_user!()
+
+        {:ok, _} =
+          Presence.track(self(), @presence, user.id, %{
+            username: user.username
+          })
+        user
+      else
+        nil
+      end
+
+    {
+      :ok,
+      socket
+      |> assign(:current_user, user)
+      |> assign(:logged_users, %{})
+      |> assign(:users, list_users())
+      |> handle_joins(Presence.list(@presence))
+    }
   end
 
   @impl true
@@ -38,6 +67,28 @@ defmodule FiarReloadedWeb.UserLive.Index do
     {:ok, _} = Users.delete_user(user)
 
     {:noreply, assign(socket, :users, list_users())}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
+    {
+      :noreply,
+      socket
+      |> handle_leaves(diff.leaves)
+      |> handle_joins(diff.joins)
+    }
+  end
+
+  defp handle_joins(socket, joins) do
+    Enum.reduce(joins, socket, fn {user, %{metas: [meta | _]}}, socket ->
+      assign(socket, :logged_users, Map.put(socket.assigns.logged_users, user, meta))
+    end)
+  end
+
+  defp handle_leaves(socket, leaves) do
+    Enum.reduce(leaves, socket, fn {user, _}, socket ->
+      assign(socket, :logged_users, Map.delete(socket.assigns.logged_users, user))
+    end)
   end
 
   defp list_users do
